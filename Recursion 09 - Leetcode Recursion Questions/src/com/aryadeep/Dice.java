c360b20d93de599f6abe8e5ccbcfcd5f5ddcf85a
Aryadeep Chakraborty <AryadeepIT@gmail.com>
2022-05-28 15:25:59 +0530

    Recursion Leetcode Q&A

---
+++ Recursion 09 - Leetcode Recursion Questions/src/com/aryadeep/Dice.java
@@ -0,0 +1,58 @@
+package com.aryadeep;
+
+import java.util.ArrayList;
+
+public class Dice {
+    public static void main(String[] args) {
+        dice("", 4);
+//        diceFace("", 4, 2);
+        System.out.println(diceRet("", 4));
+//        System.out.println(diceFaceRet("", 4,3));
+    }
+    static void dice(String p, int target){
+        if(target == 0){
+            System.out.println(p);
+            return;
+        }
+        for (int i = 1; i <= 6 && i <= target; i++) {
+            dice(p+i, target-i);
+        }
+    }
+
+    static void diceFace(String p, int target, int face){
+        if(target == 0){
+            System.out.println(p);
+            return;
+        }
+        for (int i = 1; i <= face && i <= target; i++) {
+            diceFace(p+i, target-i, face);
+        }
+    }
+
+
+    static ArrayList<String> diceRet(String p, int target){
+        if(target == 0){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 1; i <= 6 && i <= target; i++) {
+            list.addAll(diceRet(p+i, target-i));
+        }
+        return list;
+    }
+
+    static ArrayList<String> diceFaceRet(String p, int target, int face){
+        if(target == 0){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 1; i <= face && i <= target; i++) {
+            list.addAll(diceFaceRet(p+i, target-i, face));
+        }
+        return list;
+    }
+}
